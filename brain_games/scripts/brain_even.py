a3c16425c5ad97d1b49bd0d885b8e8da2d4d08d3
alexander_derevyagin <sport.master15@gmail.com>
2022-02-20 12:29:46 +0300

    upgrade file brain_even.py and fixing linter errors

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,6 +2,21 @@
 
 
 from brain_games.cli import welcome_user
+from random import randint
+
+
+def make_question_answer():
+    question_answer = {}
+    n = 3
+    for i in range(n):
+        number = randint(1, 99)
+        while number in question_answer:
+            number = randint(1, 99)
+        if number % 2 == 0:
+            question_answer[number] = 'yes'
+        else:
+            question_answer[number] = 'no'
+    return question_answer
 
 
 def main():
@@ -9,7 +24,7 @@ def main():
     name = welcome_user()
     print('Hello, ' + name + '!')
     print('Answer "yes" if number is even, otherwise answer "no".')
-    question_answer = {15 : 'no', 6 : 'yes', 7 : 'no'}
+    question_answer = make_question_answer()
     answer_count = 0
     for key in question_answer:
         print('Question: ' + str(key))
@@ -18,11 +33,13 @@ def main():
             print('Correct!')
             answer_count += 1
         else:
-            print('\'' + answer + '\' is wrong answer ;(. Correct answer was \'' + question_answer[key] + '\'.')
+            print('\'' + answer + '\' is wrong answer ;(.'
+                  'Correct answer was \'' + question_answer[key] + '\'.')
             print('Let\'s try again, ' + name + '!')
             break
     if answer_count == len(question_answer):
         print('Congratulations, ' + name + '!')
+
 
 if __name__ == '__main__':
     main()
