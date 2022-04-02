744779d4df1b9cabde9588c11010e49e3afc918a
alexander_derevyagin <sport.master15@gmail.com>
2022-04-02 12:12:28 +0300

    fixing engine and calc

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,17 +5,13 @@ import prompt
 def run_game(game):
     name = welcome_user()
     print(game.DESCRIPTION)
-    questions = set()
     round_count = 3
     for i in range(round_count):
         question, correct_answer = game.get_question_and_answer()
-        while question in questions:
-            question, correct_answer = game.get_question_and_answer()
         print('Question: {}'.format(question))
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
-            questions.add(question)
         else:
             print('\'{}\' is wrong answer ;(. '
                   'Correct answer was \'{}\'.'.format(answer, correct_answer))
